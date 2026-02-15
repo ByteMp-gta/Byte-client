db9a93daaf9083bb5c0c4f992745749a217e4544
luisOtavio13 <roberdoogarcia@gmail.com>
2026-02-15 17:41:29 -0300

    test

--- build.bat
+++ build.bat
@@ -1,4 +1,11 @@
 @echo off
-call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-msbuild "%~dp0MyPlugin.sln" /m "/p:Configuration=Debug" "/p:Platform=GTA-SA"
+if defined VSINSTALLDIR (
+    call "%VSINSTALLDIR%\VC\Auxiliary\Build\vcvars64.bat"
+) else (
+    call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat"
+    echo VSINSTALLDIR not defined, assuming msbuild is in PATH...
+)
+
+
+msbuild "%~dp0MyPlugin.vcxproj" /m /p:Configuration=DebugGTA-SA /p:Platform=Win32
